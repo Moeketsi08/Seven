fd25322b51a4f5aa30ebea6caf143873dc455170
SamMahara <mzamomahara@gmail.com>
2024-10-02 10:24:00 +0200

    Reset migrations and update school management views to use correct models

--- schoolmanagement/views.py
+++ schoolmanagement/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-import student
-import teacher
+from academic import models as academic_models
+#import student
+from teacher import models as teacher_models
 #import employee
-import academic
+#import academic
 
 @login_required(login_url='login')
 def home_page(request):
-    total_student = student.models.AcademicInfo.objects.count() # Needs to be accessed differently from the student in academic appp
-    total_teacher = teacher.models.PersonalInfo.objects.count()
-    #total_employee = employee.models.PersonalInfo.objects.count()
-    total_class = academic.models.ClassRegistration.objects.count()
+    total_student = academic_models.Student.objects.count() # Needs to be accessed differently from the student in academic appp
+    total_teacher = teacher_models.Teacher.objects.count()
+    #total_employee = employee_models.PersonalInfo.objects.count()
+    total_class = academic_models.ClassRegistration.objects.count()
     context = {
         'student': total_student,
         'teacher': total_teacher,
