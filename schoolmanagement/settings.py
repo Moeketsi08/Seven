783108bad0f39a08005430ecde6390e0aad75304
Moeketsi <moeketsi@royalmcgrady.co.za>
2024-11-22 10:41:51 +0200

    Add domain to CSRF_TRUSTED_ORIGINS

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -35,6 +35,9 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://kutlwanong-a2fbezdqguc4b6hm.southafricanorth-01.azurewebsites.net',
+]
 
 
 # Application definition
