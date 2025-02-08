db366f544b33ab1e22fe5d38f1637394e79e050e
Moeketsi <moeketsi@royalmcgrady.co.za>
2025-02-08 20:06:41 +0200

    first12345678

--- schoolmanagement/deployment.py
+++ schoolmanagement/deployment.py
@@ -22,8 +22,8 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-connection_string = os.environ['DATABASE_CONNECTIONS']
-parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
+#connection_string = os.environ['DATABASE_CONNECTIONS']
+#parameters = {pair.split('=')[0]: pair.split('=')[1] for pair in connection_string.split(' ')}
 # connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONS']
 #parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
 
